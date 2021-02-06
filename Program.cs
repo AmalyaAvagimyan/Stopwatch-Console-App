d9606792407f108ece15421792fcac847cc80534
Amalya Avagimyan <amalya_avagimyan16@alumni.aua.am>
2021-02-05 22:41:58 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace StopWatchApp
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            var stopwatch = new Stopwatch();
+     
+            Console.WriteLine(@"Type 'start' to start the stopwatch, 'stop' to stop it or ''quit' to close the program");
+
+            var input = Console.ReadLine().ToLower();
+
+            while (true)
+            {
+                if (input == "start")
+                {
+                    stopwatch.Start();
+                    Console.WriteLine(@"Type 'stop' to stop the stopwatch or 'quit' to close the program");
+                    input = Console.ReadLine();
+                }
+                else if (input == "stop")
+                {
+                    stopwatch.Stop();
+                    var duration = stopwatch.Duration();
+                    Console.WriteLine("The duration was {0}", duration);
+                    Console.WriteLine(@"Type 'start'  to restart or 'quit' to end the program");
+                    input = Console.ReadLine();           
+                }
+                else if (input == "quit")
+                {             
+                 break;
+                }
+            }
+        }
+    }
+}
---
+++ Stopwatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StopWatchApp
+{
+    public class Stopwatch
+    {
+        private bool _isRunning = false;
+        private DateTime _startingTime;
+        private DateTime _endingTime;
+        public void Start()
+        {
+            if (_isRunning == true)
+            {
+               throw new InvalidOperationException("Stopwatch is already started");
+            }
+           
+                _startingTime = DateTime.Now;
+                _isRunning = true;           
+        }
+
+        public void Stop()
+        {
+            _isRunning = false;
+            _endingTime = DateTime.Now;
+        }
+
+        public TimeSpan Duration()
+        {
+            return _endingTime - _startingTime;
+        }
+    }
+}
